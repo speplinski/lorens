602fdeb61aaaee7bd282143e2066bd25b1787884
Szymon P. Peplinski <szymon@generatywnie.com>
2015-04-10 10:00:24 +0200

    initial import

---
+++ app/nRF8001-Swift/ViewController.swift
@@ -0,0 +1,163 @@
+//
+//  ViewController.swift
+//  nRF8001-Swift
+//
+//  Created by Michael Teeuw on 31-07-14.
+//  Copyright (c) 2014 Michael Teeuw. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController, NRFManagerDelegate {
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    
+    var nrfManager:NRFManager!
+    //var feedbackView = UITextView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        nrfManager = NRFManager(
+            onConnect: {
+                self.log("C: ★ Connected")
+            },
+            onDisconnect: {
+                self.log("C: ★ Disconnected")
+            },
+            onData: {
+                (data:NSData?, string:String?)->() in
+                self.log("C: ⬇ Received data - String: \(string) - Data: \(data)")
+            },
+            autoConnect: true
+        )
+
+        nrfManager.verbose = true
+        nrfManager.delegate = self
+
+        setupUI()
+    }
+    
+    var timer1 = NSTimer();
+    var timer2 = NSTimer();
+    var timer3 = NSTimer();
+    var timer4 = NSTimer();
+    
+    func onPress1()
+    {
+        timer1 = NSTimer.scheduledTimerWithTimeInterval( 0.1, target:self, selector: Selector("sendData1"), userInfo: nil, repeats: true )
+    }
+    
+    func onPress2()
+    {
+        timer2 = NSTimer.scheduledTimerWithTimeInterval( 0.1, target:self, selector: Selector("sendData2"), userInfo: nil, repeats: true )
+    }
+    
+    func onPress3()
+    {
+        timer3 = NSTimer.scheduledTimerWithTimeInterval( 0.1, target:self, selector: Selector("sendData3"), userInfo: nil, repeats: true )
+    }
+    
+    func onPress4()
+    {
+        timer4 = NSTimer.scheduledTimerWithTimeInterval( 0.1, target:self, selector: Selector("sendData4"), userInfo: nil, repeats: true )
+    }
+    
+    func onRelease1()
+    {
+        timer1.invalidate()
+    }
+    
+    func onRelease2()
+    {
+        timer2.invalidate()
+    }
+    
+    func onRelease3()
+    {
+        timer3.invalidate()
+    }
+    
+    func onRelease4()
+    {
+        timer4.invalidate()
+    }
+    
+    func sendData1()
+    {
+        let string = "1"
+        let result = self.nrfManager.writeString(string)
+        log("⬆ Sent string: \(string) - Result: \(result)")
+    }
+    
+    func sendData2()
+    {
+        let string = "2"
+        let result = self.nrfManager.writeString(string)
+        log("⬆ Sent string: \(string) - Result: \(result)")
+    }
+    
+    func sendData3()
+    {
+        let string = "3"
+        let result = self.nrfManager.writeString(string)
+        log("⬆ Sent string: \(string) - Result: \(result)")
+    }
+    
+    func sendData4()
+    {
+        let string = "4"
+        let result = self.nrfManager.writeString(string)
+        log("⬆ Sent string: \(string) - Result: \(result)")
+    }
+}
+
+extension ViewController
+{
+    func nrfDidConnect(nrfManager:NRFManager)
+    {
+        self.log("D: ★ Connected")
+    }
+    
+    func nrfDidDisconnect(nrfManager:NRFManager)
+    {
+        self.log("D: ★ Disconnected")
+    }
+    
+    func nrfReceivedData(nrfManager:NRFManager, data: NSData?, string: String?) {
+        self.log("D: ⬇ Received data - String: \(string) - Data: \(data)")
+    }
+}
+
+extension ViewController {
+    func setupUI()
+    {
+        button1.addTarget(self, action: "onPress1", forControlEvents: UIControlEvents.TouchDown)
+        button1.addTarget(self, action: "onRelease1", forControlEvents: UIControlEvents.TouchUpInside)
+        button1.addTarget(self, action: "onRelease1", forControlEvents: UIControlEvents.TouchUpOutside)
+        
+        button2.addTarget(self, action: "onPress2", forControlEvents: UIControlEvents.TouchDown)
+        button2.addTarget(self, action: "onRelease2", forControlEvents: UIControlEvents.TouchUpInside)
+        button2.addTarget(self, action: "onRelease2", forControlEvents: UIControlEvents.TouchUpOutside)
+        
+        button3.addTarget(self, action: "onPress3", forControlEvents: UIControlEvents.TouchDown)
+        button3.addTarget(self, action: "onRelease3", forControlEvents: UIControlEvents.TouchUpInside)
+        button3.addTarget(self, action: "onRelease3", forControlEvents: UIControlEvents.TouchUpOutside)
+        
+        button4.addTarget(self, action: "onPress4", forControlEvents: UIControlEvents.TouchDown)
+        button4.addTarget(self, action: "onRelease4", forControlEvents: UIControlEvents.TouchUpInside)
+        button4.addTarget(self, action: "onRelease4", forControlEvents: UIControlEvents.TouchUpOutside)
+    }
+    
+    func log(string:String)
+    {
+        println(string)
+    }
+    
+}
+
---
+++ arduino/arduino.ino
@@ -0,0 +1,159 @@
+#include <SPI.h>
+#include "Adafruit_BLE_UART.h"
+#include <Servo.h>
+
+#define ZERO 93
+#define SPEED 80
+
+#define ADAFRUITBLE_REQ 10
+#define ADAFRUITBLE_RDY 2   // This should be an interrupt pin, on Uno thats #2 or #3
+#define ADAFRUITBLE_RST 9
+ 
+Adafruit_BLE_UART BTLEserial = Adafruit_BLE_UART( ADAFRUITBLE_REQ, ADAFRUITBLE_RDY, ADAFRUITBLE_RST );
+
+Servo sA;
+Servo sB;
+Servo sC;
+Servo sD;
+
+boolean closingA = true;
+boolean closingB = true;
+boolean closingC = true;
+boolean closingD = true;
+
+void setup()
+{
+  Serial.begin( 9600 );
+ 
+  BTLEserial.begin();  
+  
+  pinMode( A0, INPUT_PULLUP );
+  pinMode( A1, INPUT_PULLUP );
+  pinMode( A2, INPUT_PULLUP );
+  pinMode( A3, INPUT_PULLUP );
+  pinMode( A4, INPUT_PULLUP );
+  pinMode( A5, INPUT_PULLUP );
+  pinMode(  7, INPUT_PULLUP );
+  pinMode(  8, INPUT_PULLUP );
+}
+
+aci_evt_opcode_t laststatus = ACI_EVT_DISCONNECTED;
+
+void loop()
+{
+  BTLEserial.pollACI();
+  
+  aci_evt_opcode_t status = BTLEserial.getState();
+  
+  if( status != laststatus )
+  {
+    if( status == ACI_EVT_DEVICE_STARTED )
+    {
+        Serial.println( F( "* Started" ) );
+    }
+    
+    if( status == ACI_EVT_CONNECTED)
+    {
+        Serial.println( F( "* Connected!" ) );
+    }
+    
+    if( status == ACI_EVT_DISCONNECTED )
+    {
+        Serial.println( F( "* Disconnected or timed out" ) );
+    }
+    
+    laststatus = status;
+  }
+  
+  if( analogRead( A1 ) < 200 )
+  {
+    closingA = true;
+    sA.detach();
+  }
+  
+  if( analogRead( A0 ) < 200 )
+  {
+    closingA = false;
+    sA.detach();
+  }
+
+  if( analogRead( A3 ) < 200 )
+  {
+    closingB = true;
+    sB.detach();
+  }
+  
+  if( analogRead( A2 ) < 200 )
+  {
+    closingB = false;
+    sB.detach();
+  }
+  
+  if ( analogRead( A5 ) < 200)
+  {
+    closingC = true;
+    sC.detach();
+  }
+  
+  if( analogRead( A4 ) < 200 )
+  {
+    closingC = false;
+    sC.detach();
+  }
+  
+  if( digitalRead( 8 ) == LOW )
+  {
+    closingD = true;
+    sD.detach();
+  }
+  
+  if( digitalRead( 7 ) == LOW )
+  {
+    closingD = false;
+    sD.detach();
+  }
+  
+  if( status == ACI_EVT_CONNECTED )
+  {
+    while( BTLEserial.available() )
+    {
+      char c = BTLEserial.read();
+      Serial.println( c );
+      
+      if( c == '1' ) // dark green
+      {
+        sD.attach( 6 );
+        if( closingD ) sD.write( ZERO - SPEED );
+        else sD.write( ZERO + SPEED );
+      }
+      
+      if( c == '2' ) // light green
+      {
+        sC.attach( 5 );
+        if( closingC ) sC.write( ZERO - SPEED );
+        else sC.write( ZERO + SPEED );
+      }
+      
+      if( c == '3' ) // red
+      {
+        sB.attach( 4 );
+        if( closingB ) sB.write( ZERO - SPEED );
+        else sB.write( ZERO + SPEED );
+      }
+      
+      if( c == '4' ) // cyan
+      {
+        sA.attach( 3 );
+        if( closingA ) sA.write( ZERO - SPEED );
+        else sA.write( ZERO + SPEED );
+      }
+      
+      delay( 40 );
+      
+      sA.detach();
+      sB.detach();
+      sC.detach();
+      sD.detach();
+    }
+  }
+}
